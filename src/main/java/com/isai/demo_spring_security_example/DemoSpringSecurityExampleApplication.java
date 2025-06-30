c023d371e94e96f09010b14aca69cac7847273f5
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-30 10:36:48 -0500

    init

---
+++ src/main/java/com/isai/demo_spring_security_example/DemoSpringSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_spring_security_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSecurityExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_spring_security_example/DemoSpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_spring_security_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
